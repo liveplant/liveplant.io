d4b6769fd2aad5fa73c4d5ff6f800aae7d6d1ada
Austin Pray <austin@austinpray.com>
2015-08-21 21:00:17 -0500

    Switched from browserify to webpack
    yeeee

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// For instructions about this file refer to
+// webpack and webpack-hot-middleware documentation
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  debug: true,
+  devtool: '#eval-source-map',
+
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    './src/js/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Liveplant',
+      template: './src/index.html',
+      inject: true
+    })
+  ],
+
+  module: {
+    loaders: [
+      {test: /\.js?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']},
+      {test: /\.scss?$/, loaders: ['style', 'css', 'sass']}
+    ]
+  }
+};
