0db9353a72f3c2248c3a98855fb1ccf72bdf062a
Austin Pray <austin@austinpray.com>
2015-09-19 11:29:47 -0500

    Fixes node dependencies

--- Vagrant-provision.sh
+++ Vagrant-provision.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # usage: Vagrant-provision.sh
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+sudo apt-get update
+echo "installing the essentials"
+sudo apt-get install -y curl git mercurial make binutils bison gcc build-essential
+
+if [ ! -d $HOME/.nvm ]; then
+  echo "fetching and installing NVM"
+  curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+fi
 
 . ~/.nvm/nvm.sh
 
-
+echo "installing node"
 nvm install 0.12
 nvm use 0.12
 nvm alias default 0.12
+
+CHDIR='cd /vagrant'
+grep -q -F "$CHDIR" $HOME/.bashrc || echo "$CHDIR" >> .bashrc
--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
 
   config.vm.network "forwarded_port", guest: 3000, host: 3000
+  config.vm.network "forwarded_port", guest: 3001, host: 3001
 
   config.vm.provision "shell", path: "Vagrant-provision.sh", privileged: false
 end
